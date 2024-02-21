7864c7a66ec1f04859de0f6339e938da56247d81
Matthew Tucker-Simmons <tucker.simmons@gmail.com>
2024-02-21 09:32:46 -0500

    first attempt at progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,47 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const heights = {
+  small: '8px',
+  medium: '12px',
+  large: '24px',
+}
+
+const radii = {
+  small: '4px',
+  medium: '8px',
+  large: '8px',
+}
+
+
+const Wrapper = styled.progress`
+  width: 370px;
+  height: ${p => heights[p.size]};
+  padding: ${p => p.size == 'large' ? '4px' : 0};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: ${p => radii[p.size]};
+
+  &::-webkit-progress-bar {
+    background: ${COLORS.gray50};
+    border-radius: 4px;
+  }
+
+  &::-webkit-progress-value {
+    background: ${COLORS.primary};
+    border-radius: 4px 0 0 4px;
+  }
+`
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Wrapper
+        size={size}
+        max="1"
+        value={value / 100}
+        aria-valuenow={value}
+    >
+    </Wrapper>
+  )
 };
 
 export default ProgressBar;
